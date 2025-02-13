58e202dc4fccdab72558fab9e9cd6d26362a57e9
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-02-13 11:12:49 -0800

    pc - initial code from Spring Initializer

---
+++ src/main/java/edu/ucsb/cs156/springreact2025/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.springreact2025.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucsb/cs156/springreact2025/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.springreact2025.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
